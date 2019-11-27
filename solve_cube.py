fdefbeed086c1bc6cf0c2dbd1990c9adab2f3be0
saptarc <saptarc@gmail.com>
2019-11-27 23:06:38 +0530

    Update solve_cube.py

--- solve_cube.py
+++ solve_cube.py
@@ -9,23 +9,24 @@ from cube_model import Rcube
 import math
 
 initial_state = {'up': np.array(['w', 'w', 'w', 'w']),
-                 'left': np.array(['g', 'g', 'g', 'y']),
-                 'front': np.array(['r', 'r', 'o', 'r']),
-                 'right': np.array(['b', 'b', 'b', 'y']),
-                 'down': np.array(['y', 'b', 'r', 'o']),
-                 'back': np.array(['g', 'y', 'o', 'o'])
+                 'left': np.array(['g', 'g', 'o', 'b']),
+                 'front': np.array(['r', 'r', 'r', 'g']),
+                 'right': np.array(['b', 'b', 'r', 'b']),
+                 'down': np.array(['y', 'y', 'g', 'o']),
+                 'back': np.array(['y', 'y', 'o', 'o'])
                  }
 
 rubik_cube = Rcube(initial_state)
 state_mode = 'pattern'
 q_table = pd.read_pickle('q_table.pkl')
 
-max_count = 1000
+max_count = 10000
 count = 0
 while not (rubik_cube.faces_solved() == 6 or count > max_count):
     try_table = 0
-    while try_table < 30:
-        rubik_cube.change_perspective_random(1)
+    while try_table < 24:
+        if try_table:
+            rubik_cube.change_perspective_random(1)
         present_state = rubik_cube.state_string(state_mode)
 
         if not q_table[q_table['state'] == present_state].empty:
@@ -40,14 +41,15 @@ while not (rubik_cube.faces_solved() == 6 or count > max_count):
             present_move_table = random.choice(rubik_cube.moves)
             try_table += 1
 
-        present_move_random = random.choice(rubik_cube.moves)
-        present_move = random.choices([present_move_table, present_move_random], weights=[4, 1])[0]
+    # ------------------ ORIENT CUBE -------------------------------------------------------------
+    print(rubik_cube.state['front'])
+    rubik_cube.paint_cube(1)
+    rubik_cube.orient(present_move_table)
+    print("step = {}, move = {}".format(count, present_move_table))
+    rubik_cube.paint_cube(1)
 
-        # ------------------ ORIENT CUBE -------------------------------------------------------------
-        rubik_cube.orient(present_move_table)
-        rubik_cube.paint_cube(1)
-        print(count)
-        count += 1
+
+    count += 1
 
 rubik_cube.paint_cube(0)
 print(count)
